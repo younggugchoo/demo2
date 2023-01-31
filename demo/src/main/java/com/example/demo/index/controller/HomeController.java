691acce407f5acf9066f3fa4ae4cf7d49348c3ed
Administrator <Administrator@DESKTOP-MTOU32D>
2023-01-31 10:25:02 +0900

    HomeController added.

---
+++ demo/src/main/java/com/example/demo/index/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(name="index")
+	public void index () {
+		
+	}
+}
